4898e7a4c3a93637877f1d06ba38d151e7cf542f
masoud8840 <masoudgharedaghi@yahoo.com>
2021-11-28 16:27:58 +0330

    Finalize

--- public/script/common.js
+++ public/script/common.js
@@ -2,14 +2,45 @@ MenuToggle = () => {
   let menuBtn = document.querySelector("#menu-toggler");
   let menu = document.querySelector(".responsive-menu");
   let isOpen = false;
+
+  menu.addEventListener("click", (event) => {
+    if (event.srcElement.classList == "menu-inner") {
+      isOpen = false;
+      menu.style.bottom = "-50%";
+    }
+  });
   menuBtn.addEventListener("click", () => {
+    console.log(isOpen);
     if (isOpen) {
       isOpen = false;
+      menu.style.bottom = "-50%";
     } else {
       isOpen = true;
+      menu.style.bottom = "0px";
     }
-    console.log(isOpen);
+  });
+};
+
+// Toggle responsive menu's dropdown
+DropdownToggle = () => {
+  let menuContainer = document.querySelector(".responsive-menu");
+  let isOpen = false;
+  let dropdownArrow = document.querySelector("#dropdown-arrow");
+  let dropdownBtn = document.querySelector(".dropdown-item");
+  let dropdown = document.querySelector(".dropdown");
+
+  dropdownBtn.addEventListener("click", (event) => {
+    if (!isOpen) {
+      isOpen = true;
+      dropdownArrow.className = "fas fa-angle-up";
+      dropdown.style.height = "initial";
+    } else {
+      isOpen = false;
+      dropdownArrow.className = "fas fa-angle-down";
+      dropdown.style.height = "0";
+    }
   });
 };
 
 MenuToggle();
+DropdownToggle();
